d197b98f4ab5d57f46c46465d719d0e6767e6a03
tlewis32-ru <tlewis32@radford.edu>
2019-02-25 22:25:48 -0600

    Create participants.c

---
+++ code/participants.c
@@ -0,0 +1,27 @@
+/**
+ * participants.c
+ *   A simple C program to print the participants from the class.
+ *   Intended as an example of updating code on GitHub.
+ */
+
+// +---------+---------------------------------------------------------
+// | Headers |
+// +---------+
+
+#include <stdio.h>
+
+// +------+------------------------------------------------------------
+// | Main |
+// +------+
+
+int 
+main (int argc, char *argv[])
+{
+  int i = 0;
+  printf ("%02d: Tracy Lewis-Williams\n", ++i);
+  
+
+
+  printf ("There are %i participants.\n", i);
+  return 0;
+} // main
