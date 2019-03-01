aaa213dbd26fac5f7fd14f55a9f25d70155f43e7
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2019-03-01 15:51:38 -0500

    L2L Sources
    L2L Sources as of 3/1/19 4pm
    -Updated js and test

--- L2L Sources/l2l.js
+++ L2L Sources/l2l.js
@@ -12,9 +12,6 @@ function adv() {
       case 1:
         document.getElementById("q1").style.display = "block";
 
-        if(document.getElementById("ques1k").checked = true) {
-          alert("YES!");
-        }
         break;
       case 2:
         document.getElementById("q1").style.display = "none";
@@ -91,15 +88,76 @@ function adv() {
         break;
       default:
         document.getElementById("q10").style.display = "none";
+
     }
   }
     //alert(qNums);
   if(qNums > 10) {
-    alert("Congratulations! Click 'OK' to view your score!");
+    alert("Congratulations! Click 'Submit' to view your score!");
     qNums = 10;
   }
 
 
   document.getElementById("qNum").innerHTML = "Question Number: " + qNums;
   count++;
+}
+
+var cCorrect = 0;
+function subQ() {
+  if(qNums == 1 ) {
+    if(document.getElementById("ques1a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 2) {
+      if(document.getElementById("ques2a").checked == true) {
+          alert("YES!");
+          cCorrect++;
+      }
+  } else if(qNums == 3) {
+    if(document.getElementById("ques3a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 4) {
+    if(document.getElementById("ques4a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 5) {
+    if(document.getElementById("ques5a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 6) {
+    if(document.getElementById("ques6a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 7 ) {
+    if(document.getElementById("ques7a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 8) {
+    if(document.getElementById("ques8a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 9) {
+    if(document.getElementById("ques9a").checked == true) {
+        alert("YES!");
+        cCorrect++;
+    }
+  } else if(qNums == 10) {
+    if(document.getElementById("ques10a").checked == true) {
+
+        cCorrect++;
+    }
+    alert("Your score is: " + cCorrect + " / 10!");
+  }
+
+  if(qNums > 10) {
+
+  }
 }
